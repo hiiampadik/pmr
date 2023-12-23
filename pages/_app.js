8a50d0e4639e934a511a70268734596a4a397eb7
Bronislav Musil <brona.musil@gmail.com>
2023-12-23 18:35:20 +0100

    GoogleAnalytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,27 @@
 import "../styles/globals.scss";
-
-import { useEffect } from "react";
 import Script from "next/script";
-import { useRouter } from "next/router";
-
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
 
-  return (
-    <>
-      {/*<Script*/}
-      {/*  id="ga"*/}
-      {/*  strategy="lazyOnload"*/}
-      {/*  src={`https://www.googletagmanager.com/gtag/js?id=G-733EGJQ16E`}*/}
-      {/*/>*/}
-      {/*<Script strategy="lazyOnload" id="ga2">*/}
-      {/*  {`*/}
-      {/*        window.dataLayer = window.dataLayer || [];*/}
-      {/*        function gtag(){dataLayer.push(arguments);}*/}
-      {/*        gtag('js', new Date());*/}
-      {/*        gtag('config', 'G-733EGJQ16E', {*/}
-      {/*            page_path: window.location.pathname,*/}
-      {/*        });*/}
-      {/*    `}*/}
-      {/*</Script>*/}
-      <Component {...pageProps} />
-    </>
-  );
+    return (
+        <>
+            <Script
+                id="ga"
+                strategy="lazyOnload"
+                src={`https://www.googletagmanager.com/gtag/js?id=G-PV0H6J5W2Y`}
+            />
+            <Script strategy="lazyOnload" id="ga2">
+                {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-PV0H6J5W2Y');
+          `}
+            </Script>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
