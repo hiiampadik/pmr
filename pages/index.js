340d52c725192633a20aea9ea1993e3a3cd81456
Bronislav Musil <brona.musil@gmail.com>
2023-12-23 14:52:06 +0100

    Draft

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,61 @@
 import Layout from "../components/Layout";
-import React from "react";
+import React, {useState} from "react";
 import client from "../client";
 import styles from '../styles/List.module.scss'
+import {useLocalStorage} from "usehooks-ts";
 
 export default function Home({data, soundtrack}) {
+
+    const [playing, setPlaying] = useState(undefined)
+    const [stats, setStats] = useLocalStorage('stats', new Array(data?.sounds.length).fill(0))
+
+    const increaseStats = (index) => {
+        let newStats = stats;
+        newStats[index] += 1;
+        setStats(newStats);
+    }
+
+    const handleClick = (index) => {
+        if (playing !== undefined){
+            let playingAudioElement = document.getElementById(playing)
+            playingAudioElement.pause();
+            playingAudioElement.currentTime = 0;
+        }
+
+        document.getElementById(index).play()
+        setPlaying(index)
+        increaseStats(index)
+    }
 
   return (
     <Layout handleClick={() => document.getElementById('soundtrack').play()}>
         <audio id={'soundtrack'} src={soundtrack.file}/>
-      <ul className={styles.listContainer}>
-        {data?.sounds.map((sound, index) => {
-          return (
-              <Line key={index} index={index} sound={sound}/>
-          )
-        })}
-      </ul>
+        <div className={styles.listContainer}>
+            {data?.sounds.map((sound, index) => {
+              return (
+                  <Line key={index} index={index} sound={sound} handleClick={handleClick} count={stats ? stats[index] : 0}/>
+              )
+            })}
+        </div>
     </Layout>
   );
 }
 
+ function Line({sound, index, handleClick, count}) {
+
+    return (
+        <div className={styles.item} onClick={() => handleClick(index)}>
+            <audio id={index} src={sound.url}/>
+            <span className={styles.caption}>{sound.caption}</span>
+            {count !== 0 &&
+            <div className={styles.number}>
+                {count}
+            </div>
+            }
+        </div>
+    )
+
+}
 
 
 export async function getStaticProps(context) {
@@ -55,23 +92,4 @@ export async function getStaticProps(context) {
     },
     revalidate: 10,
   };
-}
-
-export function Line({sound, index}) {
-
-    return (
-        <li>
-            <audio id={index} src={sound.url}/>
-
-            <div className={styles.play} onClick={() => document.getElementById(index).play()}>
-            </div>
-            {/*<div className={styles.author}>*/}
-            {/*    {sound.author === 'other' && '-'}*/}
-            {/*    {sound.author === 'hanak' && 'Hanák'}*/}
-            {/*    {sound.author === 'xzibit' && 'XZibit'}*/}
-            {/*</div>*/}
-            <div className={styles.caption}>{sound.caption}</div>
-        </li>
-    )
-
 }
