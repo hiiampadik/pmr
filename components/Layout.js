b972bc0f8c9bf173232da3191c1445d302c46618
Bronislav Musil <brona.musil@gmail.com>
2023-12-23 19:55:04 +0100

    Favicons

--- components/Layout.js
+++ components/Layout.js
@@ -19,6 +19,11 @@ export default function Layout({
           content="pimp maj rajt"
         />
         <link rel="stylesheet" href="https://use.typekit.net/ynq5zhh.css" />
+
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png"/>
+          <link rel="manifest" href="/favicon_io/site.webmanifest"/>
       </Head>
       <TopMenu handleClick={() => handleClick()}/>
       <main>{children}</main>
